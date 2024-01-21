3fb41be8594d87ffb004b5b45ccf12b0a38fe35b
Nyikonyuk Gergő <nyikonyukgergo@gmail.com>
2024-01-21 19:34:23 +0100

    404 oldal

---
+++ views/404.php
@@ -0,0 +1,32 @@
+<?php require_once("setup.php"); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+
+    <!-- style -->
+    <link rel="stylesheet" href="<?php echo(BASE_URL); ?>/assets/css/style.css">
+    <!-- title -->
+    <title>A keresett oldal nem található | Csipcsirip - Horgobox</title>
+</head>
+
+<body>
+    <main class="center">
+        <div class="login-container">
+            <div class="logo-container">
+                <img src="<?php echo(BASE_URL); ?>/assets/images/static/csipcsiripp_logo.png" alt="csipcsiripp logo">
+            </div>
+            <h1 class="center">A keresett oldal nem található!</h1>
+            <a href="/" class="btn">Vissza a főoldalra</a>
+        </div>
+    </main>
+</body>
+
+</html>
